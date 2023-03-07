a42e9b300f88afb3f9a5e38107279c4fb1688ce1
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-07 12:53:50 +0000

    feat: Implement GET request for cofeelover endpoint and test

--- src/controller/coffeelover_request.test.ts
+++ src/controller/coffeelover_request.test.ts
@@ -3,7 +3,7 @@ import { app } from '../app';
 
 describe('Test coffeelover API endpoint request', () => {
     test('GET should return correct message', async () => {
-        const res = await request(app).get('/');
+        const res = await request(app).get('/coffeelover');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toEqual('I like coffee!');
     });
